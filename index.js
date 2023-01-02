674be6782818d3f7d137b730d3268cf566d0f118
SonJava10Rikkei <sonjapan7@gmail.com>
2023-01-02 09:32:45 +0700

    a

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/trang-chu", (req, res) => {
+app.get("/tin-tuc", (req, res) => {
+    var a = 1;
+    var b = 2;
+
+    var c = a + b;
     res.send("Hello World! I'm Son");
 });
 
